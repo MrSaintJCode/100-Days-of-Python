04f9f986a0051c7c4f10edad781f0cc9460c9e1a
Justin St-Laurent <jstlaurent@seedbox.com>
2020-01-25 15:31:33 -0500

    Second Day

---
+++ 2/Number Guessing.py
@@ -0,0 +1,66 @@
+import random
+import time
+import sys
+
+minNum = 1
+maxNum = 20
+lives = 10
+
+print("""
+              _,;;.,_
+     ,-;;,_  ;,',,,'''@
+  ,;;``  `'\ |//``-:;,.
+@`         ;^^^;      `'@
+           :@ @:
+           \ u /
+  ,=,------)^^^(------,=,
+  '-'-----/=====\-----'-'  
+          \_____/              
+      '`\ /_____                    
+      `\  \_____/_
+        \//_____\/|
+        |        ||
+        |        |'
+        :________:`
+
+--->  Guess The Number  <---
+""")
+
+number = random.randint(minNum,maxNum)
+
+while lives > 0:
+    try:
+        print("")
+        print(f"Lives - {lives}")
+        try:
+            guess = int(input(f"I am thinking of a number between {minNum} and {maxNum}\nGuess: "))
+            if guess < minNum or guess > maxNum:
+                print("")
+                print(f"[X] The number must be between {minNum} and {maxNum}")
+            else:
+                if guess == number:
+                    print("")
+                    print("--> Winner Winner Chicken Dinner <--")
+                    print(f"The number was {number}")
+                    sys.exit()
+                elif guess > number:
+                    print("")
+                    print(f"The number is LOWER than {guess}")
+                    lives = lives - 1
+                elif guess < number:
+                    print("")
+                    print(f"The number is HIGHER than {guess}")
+                    lives = lives - 1
+                else:
+                    print("")
+                
+        except ValueError:
+            print("")
+            print("[X] Please enter a number")
+            
+    
+    except KeyboardInterrupt:
+        print("")
+        print("[X] Closing...")
+        sys.exit()
+    
