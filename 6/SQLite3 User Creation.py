b163731cae98c1d2bd156631e6e45a2a2d069662
Justin St-Laurent <jstlaurent@pop-os.localdomain>
2020-02-01 09:55:02 -0500

    Minor Changes

--- 6/SQLite3 User Creation.py
+++ 6/SQLite3 User Creation.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os.path
 import time
+import sys
 
 dbName = "users"
 table = "user"
@@ -10,17 +11,23 @@ with sqlite3.connect(f"{dbName}.db") as db:
 
 
 while True:
-    print("")
-    print("<---- New User ---->")
-    first_name = input("First Name: ")
-    last_name = input("Last Name: ")
-    username = input("Username: ")
-    password = input("Password: ")
+    try:
+        print("")
+        print("<---- New User ---->")
+        first_name = input("First Name: ")
+        last_name = input("Last Name: ")
+        username = input("Username: ")
+        password = input("Password: ")
+    except KeyboardInterrupt:
+        print("")
+        print("[X] Closing...")
+        sys.exit()
+
     
     
 
     cursor.execute(f"""
-    INSERT INTO {table}(username,password,first_name,last_name)
+    INSERT INTO {table}(username,password,FirstName,LastName)
     VALUES("{username}","{password}","{first_name}","{last_name}")
     """)
     db.commit()
