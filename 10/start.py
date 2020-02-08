c0bde582618269f030f2edef3d71050febd5b224
Justin St-Laurent <jstlaurent@pop-os.localdomain>
2020-02-08 10:11:21 -0500

    Tkinter Login System

--- 10/start.py
+++ 10/start.py
@@ -3,7 +3,24 @@ import time,sys,datetime
 import sqlite3
 import os.path
 
-database = "Authentication.db"
+global database
+
+database = "Database/Authentication.db"
+
+################################
+# Authentication Table Set up
+# Table - Users
+#[+] Creating Table
+#userID
+#FirstName
+#LastName
+#Username
+#Password
+#Email
+################################
+
+print("")
+print("------- BEHIND THE SCENES -------")
 
 def register():
     global registerPAN
@@ -18,7 +35,7 @@ def register():
 
     # Create a new user
     def newUser():
-
+        database = "Database/Authentication.db"
         # Grabbing User Info
         FirstNameInfo = FirstName.get()
         LastNameInfo = LastName.get()
@@ -41,18 +58,75 @@ def register():
             cursor = db.cursor()
 
         find_user = ("SELECT * FROM Users WHERE Username = ?")
-        cursor.execute(find_user,[(Username)])
+        cursor.execute(find_user,[(UsernameInfo)])
         results = cursor.fetchall()
         if results:
-            userexistPAN = tk.Tk(className="Error")
+            print("")
+            print("* User already exist")
+            userexistPAN = tk.Tk(className=" Error")
 
+            userexistLBL = tk.Label(userexistPAN,text="User Already Exist")
+            userexistBTN = tk.Button(userexistPAN,text="Ok", command=userexistPAN.destroy)
+            userexistLBL.pack()
+            userexistBTN.pack()
             userexistPAN.mainloop()
         else:
             # Create the user
-            if Password == ConfirmPassword:
-                print("Good")
+            if PasswordInfo == ConfirmPasswordInfo:
+                print("")
+                print(f"* Creating user {UsernameInfo} in the {database}")
+                cursor.execute(f"""
+                INSERT INTO Users(FirstName,LastName,Username,Password,Email)
+                VALUES("{FirstNameInfo}","{LastNameInfo}","{UsernameInfo}","{PasswordInfo}","{EmailInfo}")
+                """)
+                db.commit()
+
+                # Checking if user has been added
+                find_user = ("SELECT * FROM Users WHERE Username = ?")
+                cursor.execute(find_user,[(UsernameInfo)])
+
+                results = cursor.fetchall()
+                if results:
+                    def reset():
+                        time.sleep(2)
+                        useraddedPAN.destroy()
+                        registerPAN.destroy()
+                        mainFUN()
+
+                    useraddedPAN = tk.Tk(className=" Successful")
+                    useraddedLBL = tk.Label(useraddedPAN,text="User has been added")
+                    useraddedBTN = tk.Button(useraddedPAN,text="Awesome!",command=reset)
+
+                    useraddedLBL.pack()
+                    useraddedBTN.pack()
+
+                    useraddedPAN.mainloop()
+                else:
+                    # User didn't add? Shouldn't ever happen.. but, you never know right?
+                    # I need to work on my naming conventions. whatisthis + error ...
+                    useraddERR = tk.Tk(className=" Error")
+                    useraddLBL = tk.Label(useraddERR,text=" An error occured")
+                    useraddBTN = tk.Button(useraddERR, text="Ok", command=useraddERR.destroy)
+
+                    useraddLBL.pack()
+                    useraddBTN.pack()
+
+                    useraddERR.mainloop()
+
+
+
+
             else:
-                print("Password doesnt match")
+                print("")
+                print("* Password doesnt match")
+                passwordERR = tk.Tk(className=" Error")
+                passwordERRLBL = tk.Label(passwordERR,text="Password Doesn't Match")
+                passwordERRBTN = tk.Button(passwordERR,text="Ok", command=passwordERR.destroy)
+
+                passwordERRLBL.pack()
+                passwordERRBTN.pack()
+                passwordERR.mainloop()
+
 
 
 
@@ -60,6 +134,7 @@ def register():
     # Back to main
     def goBack():
         registerPAN.destroy()
+        mainFUN()
 
 
     
@@ -134,10 +209,93 @@ def login():
     global loginPAN
     main.destroy()
 
+    def goBack():
+        loginPAN.destroy()
+        mainFUN()
+
+    def loginEXE():
+        global loginPAN
+        print("")
+        print("* Checking if user exist")
+        UsernameInfo = loginUsernameEntry.get()
+        PasswordInfo = loginPasswordEntry.get()
+
+        def loginSuccess():
+            userexistPAN.destroy()
+
+
+        with sqlite3.connect(database) as db:
+            cursor = db.cursor()
+
+        find_user = ("SELECT * FROM Users WHERE username = ? AND password = ?")
+        cursor.execute(find_user,[(UsernameInfo),(PasswordInfo)])
+        results = cursor.fetchall()
+
+        if results:
+            print("")
+            print(f"* {UsernameInfo} exist")
+            time.sleep(2)
+            userexistPAN = tk.Tk(className=" Successful")
+            userexistLBL = tk.Label(userexistPAN,text="Login Successful")
+            userexistBTN = tk.Button(userexistPAN,text="Woo!", command=userexistPAN.destroy)
+
+            userexistLBL.pack()
+            userexistBTN.pack()
+
+            userexistPAN.mainloop()
+        else:
+            print("")
+            print(f"* {UsernameInfo} doesnt exist")
+            userexistPAN = tk.Tk(className=" Error")
+            userexistLBL = tk.Label(userexistPAN,text="Username or Password Invalid")
+            userexistBTN = tk.Button(userexistPAN,text="Ok",command=loginSuccess)
+
+            userexistLBL.pack()
+            userexistBTN.pack()
+
+            userexistPAN.mainloop()
+
+        
+    loginPAN = tk.Tk(className=" Login")
+
+    # Username
+    loginUsernameLBL = tk.Label(loginPAN,text="Username")
+    loginUsernameEntry = tk.Entry(loginPAN)
+
+    # Password
+    loginPasswordLBL = tk.Label(loginPAN,text="Password")
+    loginPasswordEntry = tk.Entry(loginPAN,show="*")
+
+    # Buttons
+    loginBTN = tk.Button(loginPAN,text="Login",command=loginEXE,width="10")
+    backBTN = tk.Button(loginPAN,text="Back",command=goBack, width="10")
+
+    # Spacing
+    nothing1 = tk.Label(loginPAN,text=" ")
+    nothing2 = tk.Label(loginPAN,text=" ")
+
+    # Grid
+    loginUsernameLBL.grid(row="1",column="1")
+    loginUsernameEntry.grid(row="1",column="2")
+    
+    loginPasswordLBL.grid(row="3",column="1")
+    loginPasswordEntry.grid(row="3",column="2")
+
+    nothing1.grid(row="2",column="1")
+    backBTN.grid(row="5",column="1")
+    loginBTN.grid(row="5",column="2")
+    nothing2.grid(row="4",column="1")
+
+    loginPAN.mainloop()
+
+
+
+
+
 
 # def forgotpas():
 
-def main():
+def mainFUN():
     global main
     
     # Main Window
@@ -156,10 +314,45 @@ def main():
 
 def db_notfound():
     def createDB():
-        print("")
+        database = "Database/Authentication.db"
+
+        table = "Users"
+        tableContent = ["userID","FirstName","LastName","Username","Password","Email"]
+
+        newTable = f"""
+        CREATE TABLE IF NOT EXISTS {table}(
+            {tableContent[0]} INTEGER PRIMARY KEY,
+            {tableContent[1]} VARCHAR(20) NOT NULL,
+            {tableContent[2]} VARCHAR(20) NOT NULL,
+            {tableContent[3]} VARCHAR(20) NOT NULL,
+            {tableContent[4]} VARCHAR(20) NOT NULL,
+            {tableContent[5]} VARCHAR(50) NOT NULL
+        )
+        """
+
+        # Creating the DB
+        database = sqlite3.connect(database)
+        db = database.cursor()
+        db.execute(newTable)
+
+        time.sleep(2)
+
+        database = "Database/Authentication.db"
+
+        if os.path.isfile(database):
+            print("")
+            print("* Database Created")
+            dbERR.destroy()
+            mainFUN()
+        else:
+            print("")
+        
+
 
     def cancel():
-        print("")
+        dbERR.destroy()
+        
+    global dbERR
 
     dbERR = tk.Tk(className=" Error - Database Missing")
 
@@ -173,7 +366,7 @@ def db_notfound():
 
     dbERR.mainloop()
 
-if os.path.isfile(f"Database/{database}"):
-    main()
+if os.path.isfile(database):
+    mainFUN()
 else:
     db_notfound()
