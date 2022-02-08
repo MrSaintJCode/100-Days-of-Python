c0fdeeb43a013d275aa190c29971c000be5c5603
Justin <34281752+MrSaintJCode@users.noreply.github.com>
2022-02-08 09:05:20 -0500

    Day 22 - Udemy

---
+++ 22/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Welcome to the Band Name Generator")
+    user_city   = input("What's the name of the city you grew up in?\n")
+    user_pet    = input("What's your pet's name?\n")
+    print(f"Your band name could be {user_city} {user_pet}")
+
+if __name__ == '__main__':
+    main()
